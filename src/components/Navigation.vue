7a1f5784c9e97b6110692819ba0f02fe8671682f
Arden007 <cloveryuno664@gmail.com>
2022-03-30 15:37:59 +0200

    update

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -60,9 +60,6 @@
 
         </ul>
 
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-        </form>
 
         <div class="text-end">
          <div class="text-end">
